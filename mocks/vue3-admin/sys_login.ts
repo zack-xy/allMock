783d5eb5d08a7459b787aa803e034b424e3b751d
zack zheng <zack_zhengxiyun@163.com>
2023-08-10 00:31:18 +0800

    wip: add generate token

--- mocks/vue3-admin/sys_login.ts
+++ mocks/vue3-admin/sys_login.ts
@@ -1,21 +1,41 @@
-export default {
-  code: 200,
-  message: '登陆成功',
-  success: true,
-  data: {
-    'times': '@increment',
-    'userId': '@pick(["111","222"])',
-    'img': '@imageBase64',
-    'img2': '@image',
-    'userName': '@first',
-    'state|1-3': 1, // 1:在职 2:离职 3:试用期
-    'sex|1': [0, 1], // 性别：0:男 1:女
-    'userEmail': '@email',
-    'deptId': ['token'],
-    'token': '@guid',
-    'role|1': [0, 1], // 用户角色：0:系统管理员 1:普通用户
-    'roleList': [],
-    'createTime': '@datetime',
-    'lastLoginTime': '@datetime',
-  },
+import type Application from 'koa'
+import jwt from 'jsonwebtoken'
+
+interface LoginParams {
+  username: string
+  password: string
+}
+
+// 导出函数，可以根据参数，动态生成一些返回
+// 生成token，过期时间是30分钟（30 * 60s）
+// 返回的对象是符合mockjs的对象，则数据还是动态假数据
+export default function response(request: Application.Request): object {
+  const { body } = request
+  const { username, password } = body as LoginParams
+  const token = jwt.sign({
+    body,
+  }, 'vue3-admin', {
+    expiresIn: 30 * 60,
+  })
+  return {
+    code: 200,
+    message: '登陆成功',
+    success: true,
+    data: {
+      'times': '@increment',
+      'userId': '@pick(["111","222"])',
+      'img': '@imageBase64',
+      'img2': '@image',
+      'userName': '@first',
+      'state|1-3': 1, // 1:在职 2:离职 3:试用期
+      'sex|1': [0, 1], // 性别：0:男 1:女
+      'userEmail': '@email',
+      'deptId': ['token'],
+      'token': token,
+      'role|1': [0, 1], // 用户角色：0:系统管理员 1:普通用户
+      'roleList': [],
+      'createTime': '@datetime',
+      'lastLoginTime': '@datetime',
+    },
+  }
 }
--- mocks/vue3-admin/sys_profile.ts
+++ mocks/vue3-admin/sys_profile.ts
@@ -1,13 +1,33 @@
-export default {
-  code: 200,
-  success: true,
-  data: {
-    role: [
-      {
-        id: 1,
-        title: '超级管理员',
+import type Application from 'koa'
+
+import jwt from 'jsonwebtoken'
+
+// 校验token是否失效
+export default function response(request: Application.Request): object {
+  try {
+    let token = ''
+    if (request.headers && request.headers.authorization)
+      token = request.headers.authorization.split(' ')[1]
+    const decoded = jwt.verify(token, 'vue3-admin')
+    return {
+      code: 200,
+      success: true,
+      data: {
+        role: [
+          {
+            id: 1,
+            title: '超级管理员',
+          },
+        ],
+        avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
       },
-    ],
-    avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-  },
+    }
+  }
+  catch (error) {
+    return {
+      code: 401,
+      success: false,
+      message: 'token已失效',
+    }
+  }
 }
