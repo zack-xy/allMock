459226c93329a0a0a2fef522071969b62e8a14a9
zack zheng <zack_zhengxiyun@163.com>
2024-02-11 21:10:42 +0800

    docs: update

---
+++ mocks/vue3-admin/article_list.ts
@@ -0,0 +1,18 @@
+export default {
+  code: 200,
+  message: '登陆成功',
+  success: true,
+  data: {
+    'list|5-15': [
+      {
+        'ranking|+1': 1,
+        'title': '@ctitle',
+        'author': '@cname',
+        'publishDate': '@datetime',
+        'desc': '@cparagraph',
+      },
+    ],
+    'total': 500,
+  },
+
+}
