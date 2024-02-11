35d98a9738e22c050aa9f0c80be7b0164b922a6f
zack zheng <zack_zhengxiyun@163.com>
2024-02-11 18:33:55 +0800

    docs: update

--- mocks/vue3-admin/sys_profile.ts
+++ mocks/vue3-admin/sys_profile.ts
@@ -6,9 +6,39 @@ import jwt from 'jsonwebtoken'
 export default function response(request: Application.Request): object {
   try {
     let token = ''
+    const { body } = request
+    const { userName } = body as { userName: string }
+    const menus = []
+    const points = []
+    if (userName === 'super-admin') {
+      // 这里不能用赋值运算符，不知道为什么
+      menus.push(...[
+        'userManage',
+        'roleList',
+        'permissionList',
+        'articleRanking',
+        'articleCreate',
+      ])
+      points.push(...[
+        'distributeRole',
+        'importUser',
+        'removeUser',
+        'distributePermission',
+      ])
+    }
+    else if (userName === 'admin') {
+      menus.push(...[
+        'userManage',
+        'articleRanking',
+        'articleCreate',
+      ])
+      points.push(...[])
+    }
+
     if (request.headers && request.headers.authorization)
       token = request.headers.authorization.split(' ')[1]
     const decoded = jwt.verify(token, 'vue3-admin')
+
     return {
       code: 200,
       success: true,
@@ -22,6 +52,10 @@ export default function response(request: Application.Request): object {
         username: 'zackzheng',
         title: '超级管理员',
         avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        permission: {
+          menus,
+          points,
+        },
       },
     }
   }
