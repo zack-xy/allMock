c17e195ea3b93e1136a1cc37265216ed0f56047d
zack zheng <zack_zhengxiyun@163.com>
2024-03-11 23:38:28 +0800

    wip: update

--- mocks/react-schedule/__get_question_list.ts
+++ mocks/react-schedule/__get_question_list.ts
@@ -1,18 +1,87 @@
-export default {
-  errno: 0,
-  data: {
-    "list|5-15": [
-      {
-        _id: '@id',
-        title: '@ctitle',
-        "isPublished|1": true,
-        "isStar|1": true,
-        answerCount: "@natural(60, 100)",
-        createdAt: '@datetime',
-        isDeleted: false
-      }
-    ],
-    total: 1000
-  },
-  msg: 'zack'
+import type Application from 'koa'
+export default function response(request: Application.Request): object {
+  const { originalUrl } = request
+  const isStar = originalUrl.indexOf('isStar=true') !== -1
+  const isDeleted = originalUrl.indexOf('isDeleted=true') !== -1
+
+
+  if(isStar) {
+    return {
+      errno: 0,
+      data: {
+        "list|5-15": [
+          {
+            _id: '@id',
+            title: '@ctitle',
+            "isPublished|1": true,
+            isStar: true,
+            answerCount: "@natural(60, 100)",
+            createdAt: '@datetime',
+            isDeleted: false
+          }
+        ],
+        total: 1000
+      },
+      msg: 'zack'
+    }
+  }
+  if(isDeleted) {
+    return {
+      errno: 0,
+      data: {
+        "list|5-15": [
+          {
+            _id: '@id',
+            title: '@ctitle',
+            "isPublished|1": true,
+            "isStar|1": true,
+            answerCount: "@natural(60, 100)",
+            createdAt: '@datetime',
+            isDeleted: true
+          }
+        ],
+        total: 1000
+      },
+      msg: 'zack'
+    }
+  }
+
+  return {
+    errno: 0,
+    data: {
+      "list|5-15": [
+        {
+          _id: '@id',
+          title: '@ctitle',
+          "isPublished|1": true,
+          "isStar|1": true,
+          answerCount: "@natural(60, 100)",
+          createdAt: '@datetime',
+          isDeleted: false
+        }
+      ],
+      total: 1000
+    },
+    msg: 'zack'
+  }
 }
+
+
+// export default {
+//   errno: 0,
+//   data: {
+//     "list|5-15": [
+//       {
+//         _id: '@id',
+//         title: '@ctitle',
+//         "isPublished|1": true,
+//         "isStar|1": true,
+//         answerCount: "@natural(60, 100)",
+//         createdAt: '@datetime',
+//         isDeleted: false
+//       }
+//     ],
+//     total: 1000
+//   },
+//   msg: 'zack'
+// }
