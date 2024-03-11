a5015348fc0b0884549324324ca5159876f91886
zack zheng <zack_zhengxiyun@163.com>
2024-03-11 22:45:43 +0800

    wip: update

---
+++ mocks/react-schedule/__get_question_list.ts
@@ -0,0 +1,18 @@
+export default {
+  errno: 0,
+  data: {
+    "list|5-15": [
+      {
+        _id: '@id',
+        title: '@ctitle',
+        "isPublished|1": true,
+        "isStar|1": true,
+        answerCount: "@natural(60, 100)",
+        createdAt: '@datetime',
+        isDeleted: false
+      }
+    ],
+    total: 1000
+  },
+  msg: 'zack'
+}
--- src/route/react-schedule.ts
+++ src/route/react-schedule.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import Router from 'koa-router'
 import questionIdFormat from 'mock/react-schedule/__get_question_id'
+import questionListFormat from 'mock/react-schedule/__get_question_list'
 import Mock, { generateNormalPost, resetRequest } from '@/utils/index'
 import { info } from '@/utils/log4j'
 
@@ -21,11 +22,21 @@ router.post('/sys/profile2', async (ctx) => {
   ctx.body = res
 })
 
+// 问卷信息
 router.get('/question/:id', async (ctx) => {
   const request = resetRequest(ctx.request)
   const { originalUrl } = request
   info(`请求接口：/${originalUrl}`)
   const res = await mock.mockData(request, { format: questionIdFormat })
+  ctx.body = res
+})
+
+// 问卷列表
+router.get('/questions/list', async (ctx) => {
+  const request = resetRequest(ctx.request)
+  const { originalUrl } = request
+  info(`请求接口：/${originalUrl}`)
+  const res = await mock.mockData(request, { format: questionListFormat })
   ctx.body = res
 })
 
