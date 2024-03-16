33a2505e262ecfc3246a0f6d3619abeb8c3d5eb2
zack zheng <zack_zhengxiyun@163.com>
2024-03-16 23:51:42 +0800

    docs: update

--- mocks/react-schedule/__get_question_id.ts
+++ mocks/react-schedule/__get_question_id.ts
@@ -2,7 +2,30 @@ export default {
   errno: 0, 
   data: {
     id: '@id',
-    title: '@ctitle'
+    title: '@ctitle',
+    componentList: [
+      // Title
+      {
+        fe_id: 'a1',
+        type: 'questionTitle',
+        title: '标题',
+        props: {text: '个人信息调研', level: 1, isCenter: false}
+      },
+      // Input
+      {
+        fe_id: 'a2',
+        type: 'questionInput',
+        title: '输入框1',
+        props: {title: '你的姓名', placeholder: '请输入姓名...'}
+      },
+      // Input
+      {
+        fe_id: 'a3',
+        type: 'questionInput',
+        title: '输入框2',
+        props: {title: '你的电话', placeholder: '请输入电话...'}
+      }
+    ]
   }, 
   msg: 'zack' 
 }
