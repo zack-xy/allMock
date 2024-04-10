7b05bf5ff821a4b208f40c51baa88df249e0c116
zack zheng <zack_zhengxiyun@163.com>
2024-04-10 23:29:50 +0800

    fix: update

--- mocks/react-schedule/__get_question_id.ts
+++ mocks/react-schedule/__get_question_id.ts
@@ -9,6 +9,7 @@ export default {
         fe_id: 'a1',
         type: 'questionTitle',
         title: '标题',
+        isHidden: false,
         props: {text: '个人信息调研', level: 1, isCenter: false}
       },
       // Input
@@ -16,6 +17,7 @@ export default {
         fe_id: 'a2',
         type: 'questionInput',
         title: '输入框1',
+        isHidden: false,
         props: {title: '你的姓名', placeholder: '请输入姓名...'}
       },
       // Input
@@ -23,6 +25,7 @@ export default {
         fe_id: 'a3',
         type: 'questionInput',
         title: '输入框2',
+        isHidden: false,
         props: {title: '你的电话', placeholder: '请输入电话...'}
       }
     ]
